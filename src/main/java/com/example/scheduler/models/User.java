4873cec6d95baaf32e928cf56993e581f63bf8bd
muhammeta7 <muhammeta7@gmail.com>
2020-06-20 20:42:03 -0400

    completed User pojo

--- src/main/java/com/example/scheduler/models/User.java
+++ src/main/java/com/example/scheduler/models/User.java
@@ -1,5 +1,50 @@
 package com.example.scheduler.models;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class User {
     // Long id, name, email, List<Appointment>
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    @OneToMany
+    private List<Appointment> appointments;
+
+    public User(){};
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Appointment> getAppointments() {
+        return appointments;
+    }
+
+    public void setAppointments(List<Appointment> appointments) {
+        this.appointments = appointments;
+    }
 }
