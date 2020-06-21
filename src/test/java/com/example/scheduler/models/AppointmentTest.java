dcf031681cdbdad1d566fc31c7ff20d92a4b233c
muhammeta7 <muhammeta7@gmail.com>
2020-06-20 23:27:03 -0400

    test for users

--- src/test/java/com/example/scheduler/models/AppointmentTest.java
+++ src/test/java/com/example/scheduler/models/AppointmentTest.java
@@ -2,9 +2,7 @@ package com.example.scheduler.models;
 
 import org.junit.Before;
 import org.junit.Test;
-
 import java.time.LocalDate;
-
 import static org.junit.Assert.*;
 
 public class AppointmentTest {
