9690d0eab7ed56d59780ab9dfd59bf4e1b7f2bfe
muhammeta7 <muhammeta7@gmail.com>
2020-06-27 16:05:00 -0400

    remove user service

--- src/main/java/com/example/scheduler/services/UserService.java
+++ src/main/java/com/example/scheduler/services/UserService.java
@@ -1,18 +1,9 @@
 package com.example.scheduler.services;
 import com.example.scheduler.models.User;
-import com.example.scheduler.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    private UserRepository userRepo;
-
-    @Autowired
-    public UserService(UserRepository userRepo) {
-        this.userRepo = userRepo;
-    }
-
-
 
 }
