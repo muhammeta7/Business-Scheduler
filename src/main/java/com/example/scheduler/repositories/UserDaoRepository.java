88b8669ca206b141ea42048369c357ca2a91d5d5
muhammeta7 <muhammeta7@gmail.com>
2020-06-27 02:42:52 -0400

    add transfer object model to dto for insertion into db

--- src/main/java/com/example/scheduler/repositories/UserDaoRepository.java
+++ src/main/java/com/example/scheduler/repositories/UserDaoRepository.java
@@ -2,6 +2,8 @@ package com.example.scheduler.repositories;
 
 import com.example.scheduler.models.DAOUser;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserDaoRepository extends CrudRepository<DAOUser, Long> {
 }
